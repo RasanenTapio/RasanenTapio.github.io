00138330689850b20bb068548ad430d99019e61a
RasanenTapio <wandrw4@gmail.com>
2025-01-27 20:59:10 +0000

    Deploying to gh-pages from @ RasanenTapio/RasanenTapio.github.io@2b5153eb9392fe353855ec156039ceef3e1e864e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,14 +443,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%74%61%70%69%6F.%72%61%73%61%6E%65%6E@%6B%65%6C%61.%66%69", "_blank");
         },
       },{
         id: 'social-rss',
