56d8da404ecf2557917745d8fbd074dae2219f61
RasanenTapio <wandrw4@gmail.com>
2025-01-29 20:12:16 +0000

    Deploying to gh-pages from @ RasanenTapio/RasanenTapio.github.io@7ae4f52cb2e6cb27f793808069a007cda20279bd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Ongoing and finished research projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -393,51 +393,36 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-country-team",
+          title: 'Country Team',
+          description: "EUROMOD update project for Finland",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-a-stress-test-for-welfare-state",
+          title: 'A stress test for welfare state',
+          description: "Stress-testing Finnish Welfare State with microsimulation",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-motherhood-and-career-penalty",
+          title: 'Motherhood and career penalty',
+          description: "How does motherhood affect women’s careers? Causal estimates using medically induced ovulation treatments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-parental-leave-and-health",
+          title: 'Parental leave and health',
+          description: "Father&#39;s Quota and Maternal Mental Health",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-private-childcare-subsidies",
+          title: 'Private childcare subsidies',
+          description: "Effects of private childcare subsidies on supply and demand of ECEC services",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-childcare-and-mental-health",
+          title: 'Childcare and mental health',
+          description: "Home care allowance use and maternal mental health",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
