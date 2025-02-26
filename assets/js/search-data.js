72eb88c2902fdd3e8ac052c4257660651d77df5a
RasanenTapio <wandrw4@gmail.com>
2025-02-26 19:49:18 +0000

    Deploying to gh-pages from @ RasanenTapio/RasanenTapio.github.io@873d8c06ffd099ca848acc0716df46fc3fdc3b64 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Curriculum vitae.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
